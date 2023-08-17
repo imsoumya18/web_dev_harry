cec271c02e29ec960bec223367df6112af1f2ebb
soumya <soumyadeep184@gmail.com>
2023-08-17 19:13:26 +0530

    tcfygv

--- node/index.js
+++ node/index.js
@@ -5,28 +5,33 @@ const { url } = require("inspector");
 const hostname = "127.0.0.1";
 const port = 3000;
 const home = fs.readFileSync("index.html");
-const about = fs.readFileSync("about.html");
-const services = fs.readFileSync("services.html");
-const contact = fs.readFileSync("contact.html");
+const about = fs.readFileSync(
+  "/home/soumya/VS Code Projects/web_dev_harry/node/about.html"
+);
+const services = fs.readFileSync(
+  "/home/soumya/VS Code Projects/web_dev_harry/node/services.html"
+);
+const contact = fs.readFileSync(
+  "/home/soumya/VS Code Projects/web_dev_harry/node/contact.html"
+);
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
-  res.end("Hello World");
 
-  //   if (url == "/") {
-  //     res.end(home);
-  //   } else if (url == "/about") {
-  //     res.end(about);
-  //   } else if (url == "/services") {
-  //     res.end(services);
-  //   } else if (url == "/contact") {
-  //     res.end(contact);
-  //   } else {
-  //     res.statusCode = 404;
-  //     res.end("<h1>404 not found</h1>");
-  //   }
+  if (req.url == "/") {
+    res.end(home);
+  } else if (req.url == "/about") {
+    res.end(about);
+  } else if (req.url == "/services") {
+    res.end(services);
+  } else if (req.url == "/contact") {
+    res.end(contact);
+  } else {
+    res.statusCode = 404;
+    res.end("<h1>404 not found</h1>");
+  }
 });
 
 server.listen(port, hostname, () => {
