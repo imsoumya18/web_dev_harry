7c3d2c47afa4049e79ebeb836639eaf612591b51
soumya <soumyadeep184@gmail.com>
2023-08-18 21:07:33 +0530

    crttygvjh

--- Dance Website/app.js
+++ Dance Website/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const app = express();
-const port = 80;
+const port = 2000;
 
 // EXPRESS SPECIFIC STUFF
 app.use("/static", express.static("/static"));
@@ -14,8 +14,7 @@ app.set("views", path.join(__dirname, "views"));
 
 // ENDPOINTS
 app.get("/", (req, res) => {
-  const con = "Best content";
-  const params = { title: "PUG is the best", content: con };
+  const params = {};
   res.status(200).render("index.pug", params);
 });
 
